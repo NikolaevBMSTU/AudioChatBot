b00bafaabf2d7f377de3f10adb9d5c9b8c5180e4
NikolaevBMSTU <denn.Nikolaev@gmail.com>
2025-11-03 17:21:03 +0300

    fixes

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes
 
 from agent import ChatBot
 
@@ -22,16 +22,18 @@ agent = ChatBot()
 
 async def llm_request(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.effective_message and update.effective_message.text:
+        logger.info("Pass request to LLM")
         answer = agent.invoke(update.effective_user.id, update.effective_message.text)
-        update.message.reply_text(answer["messages"][-1].content)
+        await update.message.reply_text(answer["messages"][-1].content)
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
 
-    app.add_handler(CommandHandler("response", llm_request))
+    app.add_handler(MessageHandler(None, callback=llm_request))
 
     app.run_webhook(
+        listen='0.0.0.0',
         port=8000,
-        webhook_url=f"{os.getenv("OWN_HOST")}{os.getenv("CONTEXT_PATH")}",
+        webhook_url=f"{os.getenv('OWN_HOST')}{os.getenv('CONTEXT_PATH')}",
         secret_token=os.getenv("TELEGRAM_WEBHOOK_SECRET_TOKEN")
     )
